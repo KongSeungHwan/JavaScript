4e5ab9ac19dd5171a71efa830a05149c9a3076b3
DESKTOP-U7PDAB6\Kosmo <tmdghks3520@nate.com>
2023-03-18 09:59:28 +0900

    no message

--- JavaScriptStudy/0317/js/cal.js
+++ JavaScriptStudy/0317/js/cal.js
@@ -1,20 +1,36 @@
-export 
-function display(va){
-    document.querySelector('#result').value += va;
+function display(val){
+    document.getElementById('result').value += val;
+    return val;
 }
+
 function solve(){
-    let t= eval(document.querySelector('#result').value);
-    document.querySelector('#result').value= t;
-    return t;
+    let x = document.getElementById('result').value;
+    let y = eval(x);
+    document.getElementById('result').value = y;
+    return y;
 }
 
 function clearScreen(){
-    document.querySelector('#result').value="";
+    document.getElementById('result').value = '';
+}
+//let f = document.querySelector('.keys').firstElementChild;
+//f.onclick=function(){display(f.value);};
+window.onload=function(){
+    let buttons=document.getElementsByClassName('button');
+    let operators=document.getElementsByClassName('operator');
+    for(let button of buttons){
+        let val=button.value;
+        if(!button.className.includes('equal-sign')){
+            button.addEventListener('click',()=>display(val));
+        }
+    }
+    for(let oper of operators){
+        let val=oper.value;
+        oper.addEventListener('click',()=>display(val));
+    }
+    document.querySelector('.equal-sign')
+        .addEventListener('click',()=>solve());
 }
 
-(function onload(){
-    document.querySelector('.keys').firstElementChild.addEventListener('click',function(){
-        /* display(document.getElementsByClassName('.button')); */
-        console.log(document.getElementsByClassName('.button'));
-    })
-})();
+//window.onload documnet.onload
+//(function(){})();
